0c04b7ed45032abaaa08bf07aa3fab09730229b2
Aleja <alejamarin2531@gmail.com>
2025-03-02 12:48:31 -0500

    inicio de proyecto

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Sec1 from './modulos/sec1';
+
+export default function Home() {
+    return (
+        <>
+            <Sec1 />
+        </>
+    ) 
+}
---
+++ src/components/modulos/sec1.jsx
@@ -0,0 +1,9 @@
+//rfc - crear la estructura basica
+import React from 'react'
+
+export default function sec1() {
+  return (
+    <div>sec1</div>
+  )
+}
+
