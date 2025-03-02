e03aab3c6baccadb569481ec01b1d86af614dc01
kevin patiño <kevin11234daniel@gmail.com>
2025-03-02 13:26:01 -0500

    create nav
    aa

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Sec1 from './modulos/sec1';
+import Nav from "./modulos/nav"
+import Sec2 from './modulos/sec2';
 
 export default function Home() {
     return (
         <>
+            <Nav />
             <Sec1 />
+            <Sec2 />
         </>
     ) 
 }
