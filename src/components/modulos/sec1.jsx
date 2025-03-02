4e49084cb33247b32d002df95e0f1465630b7be0
Aleja <alejamarin2531@gmail.com>
2025-03-02 17:45:07 -0500

    seccion 1
    aa

--- src/components/modulos/sec1.jsx
+++ src/components/modulos/sec1.jsx
@@ -1,9 +1,34 @@
 //rfc - crear la estructura basica
 import React from 'react'
+import '../../estilos/sec1.css';
+import logo from '../../estilos/img/sec1.png';
+import text from '../../estilos/img/alkeem.png';
+
 
 export default function sec1() {
   return (
-    <div>sec1</div>
+    <>
+    <div
+      className="container-fluid p-4 border"
+      style={{
+        background:
+          "linear-gradient(90deg, #EFD7EF 8%, #F5F9FC 39%, #F8EAE1 66%, #EAF8F9 91%)",
+      }}
+    >
+      <div className="row align-items-center text-center">
+        
+        <div className="col-lg-6 col-md-6 col-sm-12 p-3 d-none d-sm-block">
+          <img src={logo} alt="Descripción" className="img-fluid" />
+        </div>
+  
+     
+        <div className="col-lg-6 col-md-6 col-sm-12 p-3">
+          <img src={text} alt="Descripción" className="img-fluid" />
+        </div>
+      </div>
+    </div>
+  </>
+  
   )
 }
 
