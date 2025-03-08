99f7cf47147460d4cb708abc57322a50a2dd4abc
kevin patiño <kevin11234daniel@gmail.com>
2025-03-07 20:47:39 -0500

    boton
    boton de pedir

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Sec1 from './modulos/sec1';
 import Nav from "./modulos/nav"
 import Sec2 from './modulos/sec2';
 
@@ -7,7 +6,6 @@ export default function Home() {
     return (
         <>
             <Nav />
-            <Sec1 />
             <Sec2 />
         </>
     ) 
