7884f9b7e0becb34603ffb568f65b091ca407843
Aleja <alejamarin2531@gmail.com>
2025-03-24 16:03:44 -0500

    seccion productos
    hice el titulo

---
+++ src/components/Productos.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../components/modulos/nav';
+import Titulo from '../components/productos/secTitulo';
+
+export default function Productos() {
+  return (
+    <>
+            <Nav/>
+            <Titulo/>
+            </>
+          
+  )
+}
---
+++ src/components/productos/secTitulo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function secTitulo() {
+  return (
+    <>
+   <div className="container-fluid p-4 border" id='tituloP'>
+    </div>
+    </>
+  )
+}
