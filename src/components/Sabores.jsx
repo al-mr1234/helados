2be3c684d1f7149e307da336e44cc8ed5b700a66
kevin patiño <kevin11234daniel@gmail.com>
2025-03-24 16:00:59 -0500

    sabores sec1

--- src/components/Sabores.jsx
+++ src/components/Sabores.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Nav from '../components/modulos/nav'
+import Sabor1 from './sabores/Sabor1';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Sabores() {
     return (
         <>
             <Nav />
+            <Sabor1 />
             </>
 
     )
